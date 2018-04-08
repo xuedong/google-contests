0a65ffbc2cf9ceaa082725300a7765b8eae8bcb8
xuedong <shang.xuedong@yahoo.fr>
2018-04-08 02:39:02 +0200

    Go Gopher version A=200

--- Code Jam 2018/Qualification Round/Go, Gopher!/go_gopher.go
+++ Code Jam 2018/Qualification Round/Go, Gopher!/go_gopher.go
@@ -60,7 +60,53 @@ func main() {
 				}
 			}
 		} else if area == 200 {
+			fmt.Printf("%d %d\n", 500, 500)
 
+			board := make([][]int, 1000)
+			for i := range board {
+				board[i] = make([]int, 1000)
+			}
+
+			for i := range board {
+				for j := range board {
+					board[i][j] = 0
+				}
+			}
+
+			startPoints := [10]int{500, 505, 510, 515, 520,
+				525, 530, 535, 540, 545}
+
+			for _, start := range startPoints {
+				// fmt.Println(start)
+				for {
+					var i, j int
+					fmt.Scanf("%d %d", &i, &j)
+					if board[i][j] == 0 {
+						board[i][j] = 1
+					}
+
+					if i == 0 && j == 0 {
+						break
+					} else {
+						// fmt.Printf("%d %d\n", 500, 500)
+						if neighbors(start, 500, board) < 8 || board[start][500] == 0 {
+							fmt.Printf("%d %d\n", start, 500)
+						} else if neighbors(start, 501, board) < 8 {
+							fmt.Printf("%d %d\n", start, 501)
+						} else if neighbors(start+1, 500, board) < 8 {
+							fmt.Printf("%d %d\n", start+1, 500)
+						} else if neighbors(start+1, 501, board) < 8 {
+							fmt.Printf("%d %d\n", start+1, 501)
+						} else if neighbors(start+2, 500, board) < 8 {
+							fmt.Printf("%d %d\n", start+2, 500)
+						} else if neighbors(start+2, 501, board) < 8 {
+							fmt.Printf("%d %d\n", start+2, 501)
+						} else {
+							break
+						}
+					}
+				}
+			}
 		}
 	}
 }
