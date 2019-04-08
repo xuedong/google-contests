b03f03125260f22cb177bde0cb76ce496cfd85f8
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-04-08 14:54:11 +0200

    Naive solution for pb 1

--- Kick Start 2019/Round A/Training/training.go
+++ Kick Start 2019/Round A/Training/training.go
@@ -2,4 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
+
+func main() {
+	var testCases int
+	fmt.Scanln(&testCases)
+
+	for testCase := 1; testCase <= testCases; testCase++ {
+		var total int
+		var picked int
+		fmt.Scanln(&total, &picked)
+
+		ratings := make([]int, total)
+		for i := range ratings {
+			fmt.Scan(&ratings[i])
+		}
+
+		// sort the rating list
+		sort.Ints(ratings)
+
+		// compute the sum of differences between the p-th element and its precedents
+		best := math.MaxInt32
+		for i := picked - 1; i < total; i++ {
+			time := 0
+			for j := i - picked + 1; j < i; j++ {
+				time += ratings[i] - ratings[j]
+			}
+			if time < best {
+				best = time
+			}
+		}
+
+		fmt.Printf("Case #%d: %d\n", testCase, best)
+	}
+}
