96f3cda153fc89c9a4a9a0a598a694d5a6ba377f
xuedong <shang.xuedong@yahoo.fr>
2019-04-13 14:04:30 +0200

    Round 1A, first trial for pb1

--- Code Jam 2019/Round 1A/Pylons/pylons.go
+++ Code Jam 2019/Round 1A/Pylons/pylons.go
@@ -11,17 +11,33 @@ import (
 // 	c int
 // }
 
-func count(row int, col int, x int, y int, occupied []int) int {
+// Contains tests if an element belongs to an int slice
+func Contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+// Count counts the number of unavailable cells in a grid
+func Count(row int, col int, x int, y int, occupied []int) (int, []int) {
+	var availableMoves []int
 	sum := 0
 	for i := 1; i <= row; i++ {
 		for j := 1; j <= col; j++ {
-			if i == x || j == y || i-j == x-y || i+j == x+y {
+			flattened := (i-1)*col + j
+			if i == x || j == y || i-j == x-y || i+j == x+y ||
+				Contains(occupied, flattened) {
 				sum++
+			} else {
+				availableMoves = append(availableMoves, flattened)
 			}
 		}
 	}
 
-	return sum
+	return sum, availableMoves
 }
 
 func main() {
@@ -46,13 +62,40 @@ func main() {
 			found := false
 
 			for found == false {
-				start := rand.Intn(length)
+				current := rand.Intn(length) + 1
 				path := make([]int, length)
-				path[0] = start
+				path[0] = current
+				pathLength := 0
+				var occupied []int
 				possible := true
 
 				for possible == true {
+					occupied = append(occupied, current)
+					y := current % col
+					if y == 0 {
+						y = col
+					}
+					x := current/col + 1
+					_, availableMoves := Count(row, col, x, y, occupied)
+					if len(availableMoves) == 0 {
+						possible = false
+					} else {
+						current = availableMoves[rand.Intn(len(availableMoves))]
+						pathLength++
+						path[pathLength] = current
+					}
+				}
 
+				if pathLength == length-1 {
+					for i := 0; i < length; i++ {
+						y := path[i] % col
+						if y == 0 {
+							y = col
+						}
+						x := path[i]/col + 1
+						fmt.Printf("%d %d\n", x, y)
+					}
+					found = true
 				}
 			}
 		}
