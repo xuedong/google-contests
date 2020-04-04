e69690c389b04b921eccf49233e81d5c709d50ab
xuedong <shang.xuedong@yahoo.fr>
2020-04-04 22:35:38 +0200

    Code Jam 2020 - esab version 1

---
+++ Code Jam 2020/Qualification Round/ESAb ATAd/esab_atad_10.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var testCases int
+	var B int
+	fmt.Scanln(&testCases, &B)
+	for testCase := 1; testCase <= testCases; testCase++ {
+		var bits bytes.Buffer
+		for i := 0; i < 150; i++ {
+			fmt.Printf("%d\n", i%10+1)
+			var rec int
+			fmt.Scanln(&rec)
+			if i >= 140 {
+				bits.WriteString(strconv.Itoa(rec))
+			}
+		}
+
+		output := bits.String()
+		fmt.Printf("%s\n", output)
+		var answer string
+		fmt.Scanln(&answer)
+		if answer == "N" {
+			break
+		}
+	}
+}
